aadd154f48e3356d43301a2c4ee185940307de31
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-12-14 20:50:36 +0900

    no image_validation

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,5 +2,5 @@ class Blog < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :title, presence: true
   validates :content, presence: true
-  validates :image, presence: true
+  # validates :image, presence: true
 end
